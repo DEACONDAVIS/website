5280c14843a5439151dfedd7ef8aada8a7609ff6
DEACONDAVIS <142839767+DEACONDAVIS@users.noreply.github.com>
2025-02-14 09:46:07 -0700

    I FIXED THE STUFF

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ function doCrazyMaths(){
     let solution = x + y
     document.getElementById("answer").innerHTML = solution
 }
+function Maths(){
+    let x = parseInt(document.getElementById("Num_1").value)
+    let y =  parseInt(document.getElementById("Num_2").value)
+    let solution = x + y
+    document.getElementById("Answers").innerHTML = solution
+}
+function M_Maths(){
+    let x = parseInt(document.getElementById("Num_1").value)
+    let y =  parseInt(document.getElementById("Num_2").value)
+    let solution = x * y
+    document.getElementById("Answers").innerHTML = solution
+}
+function D_Maths(){
+    let x = parseInt(document.getElementById("Num_1").value)
+    let y =  parseInt(document.getElementById("Num_2").value)
+    let solution = x / y
+    document.getElementById("Answers").innerHTML = solution
+}
+function S_Maths(){
+    let x = parseInt(document.getElementById("Num_1").value)
+    let y =  parseInt(document.getElementById("Num_2").value)
+    let solution = x - y
+    document.getElementById("Answers").innerHTML = solution
+}
+function randomPlaceholder(){
+    return Math.floor(Math.random() * 10) + 1;
+}
+window.onload = function(){
+    document.getElementById("Num_1").value = randomPlaceholder()
+    document.getElementById("Num_2").value = randomPlaceholder()
+}
